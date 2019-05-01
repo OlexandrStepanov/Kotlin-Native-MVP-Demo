347836f164e06357b789977cbaafb61fc26a812f
Oleksandr Stepanov <Oleksandr.Stepanov@akqa.com>
2019-05-01 16:14:46 +0300

    Configure Firebase in Android app

--- androidApp/src/main/kotlin/DemoApplication.kt
+++ androidApp/src/main/kotlin/DemoApplication.kt
@@ -6,4 +6,10 @@ import com.akqa.kn.lib.*
 class DemoApplication : Application() {
     val locationService by lazy { LocationServiceImpl(this) }
     val wikiRepository = WikiRepositoryImpl()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseService.configure()
+    }
 }
